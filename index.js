ac47093d6eae0a51092d3d4d824e6b80a4161c7b
saravanakumar.v <saravanakumar.velayutham@mavs.uta.edu>
2019-09-26 10:50:15 -0500

    working initial angular heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 const path = require('path');
 var app = express()
 
-const angularPath = '/SelfHostedAssistant/dist'
+const angularPath = '/SelfHostedAssistant/dist/SelfHostedAssistant'
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + angularPath))
 
