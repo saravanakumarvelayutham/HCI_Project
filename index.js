329a22089b355b1a2c89d178b2734532588d52ee
saravanakumar.v <saravanakumar.velayutham@mavs.uta.edu>
2019-09-26 09:13:12 -0500

    Added initial angular application

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-print('hey')
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + 'SelfHostedAssistant/dist'))
+
+app.get('/', function(request, response) {
+  response.send('Self-Hosted Assistant Landing Pages!')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
