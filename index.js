f3a3b14ed0e5f98d52183dbf9be6557e97bbfacb
saravanakumar.v <saravanakumar.velayutham@mavs.uta.edu>
2019-09-26 09:29:08 -0500

    post install script corrected

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express()
 
-const angularPath = '/SelfHostedAssistant/dist/SelfHostedAssistant'
+const angularPath = '/SelfHostedAssistant/dist'
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + angularPath))
 
