9b50ef8fbf86bd053279cabd1f5872294f417db7
saravanakumar.v <saravanakumar.velayutham@mavs.uta.edu>
2019-10-06 12:13:27 -0500

    calendar layout added
    added initial basic calendar display layout

--- SelfHostedAssistant/src/app/app.component.ts
+++ SelfHostedAssistant/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { CalendarEvent } from 'angular-calendar';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'SelfHostedAssistant';
+  view: string = 'month';
+
+  viewDate: Date = new Date();
+
+  events: CalendarEvent[] = [];
 }
--- SelfHostedAssistant/src/app/app.module.ts
+++ SelfHostedAssistant/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { DemoUtilsModule } from './demo-utils/module';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory
+    }),
+    DemoUtilsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
